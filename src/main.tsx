90b72e7f1e9895c97126c5912fa8dd95def788f9
Migueltri <mitrijurrea@gmail.com>
2025-10-14 20:44:35 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,11 @@
-import { StrictMode } from 'react'
-import './i18n'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/page";
-import Noticia from "./pages/noticia/Noticia";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./i18n";
+import "./index.css";
+import App from "./App"; // importa tu componente principal
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/noticia/:id" element={<Noticia />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
