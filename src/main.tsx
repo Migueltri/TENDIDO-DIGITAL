b72cba549ec4249e9f42b46dccdb68376b57b627
Migueltri <mitrijurrea@gmail.com>
2025-10-14 20:39:59 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import './i18n'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/page";
+import Noticia from "./pages/noticia/Noticia";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/noticia/:id" element={<Noticia />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
