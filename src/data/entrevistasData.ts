f0548a7fd144c828dddaf3c174765de0a433112b
Migueltri <mitrijurrea@gmail.com>
2025-12-09 19:29:33 +0100

    Create entrevistasData.ts

---
+++ src/data/entrevistasData.ts
@@ -0,0 +1,65 @@
+// src/data/entrevistasData.ts
+
+export const entrevistasData = [
+  {
+    id: 500,
+    slug: "david-galvan",
+    title:
+      "“Expreso mi concepto bajo los cánones del clasicismo, con mi singularidad personal” - Entrevista a David Galván",
+    image: "/images/entrevista.jpg",
+    category: "Entrevistas",
+    date: "29 de noviembre de 2025",
+    fullContent: `*David Galván* encara uno de los momentos más determinantes ... (todo tu texto aquí)`,
+    footerImage1: "/images/victorluengo.jpg",
+    footerImage1Caption: "Fotos de Víctor Luengo",
+    footerImage2: "/images/davidgalvan3.jpg",
+    author: "Eduardo Elvira",
+    authorLogo: "/images/edu4.jpg",
+    showAuthorHeader: true,
+  },
+  {
+    id: 501,
+    slug: "tomas-gonzalez",
+    title:
+      "“Busco torear lo más despacio posible” - Entrevista al novillero Tomás González",
+    image: "images/tomasgonzalez.jpg",
+    category: "Entrevistas",
+    date: "14 de octubre de 2025",
+    fullContent: `La temporada del joven novillero alcorisano Tomás González ... (texto completo)`,
+  },
+  {
+    id: 502,
+    slug: "carlos-zuniga",
+    title:
+      "Entrevista a Carlos Zúñiga: “Soy una persona ambiciosa y la vida me ha enseñado a saber esperar”",
+    image: "images/carloszuñiga.jpg",
+    category: "Entrevistas",
+    date: "17 de octubre de 2025",
+    fullContent: `Carlos, en un momento donde ... (texto completo)`,
+  },
+  {
+    id: 503,
+    slug: "enrique-martinez-chapurra",
+    title:
+      "“Soy torero no sobresaliente” - Entrevista con Enrique Martínez Chapurra",
+    image: "images/enriquez.jpg",
+    category: "Entrevistas",
+    date: "1 de noviembre de 2025",
+    fullContent: `Matador de toros desde 2003 ... (texto completo)`,
+  },
+  {
+    id: 504,
+    slug: "sergio-rodriguez",
+    title:
+      "“Considero que soy un torero que tiene personalidad” - Entrevista con Sergio Rodríguez",
+    image: "/images/sergior.jpg",
+    imageCaption: "Sergio Rodríguez en la Final de la Copa Chenel",
+    footerImage1: "/images/sergior1.jpg",
+    footerImage1Caption:
+      "Sergio Rodríguez el pasado 12 de Octubre en Las Ventas - Foto Plaza 1",
+    footerImage2: "/images/sergior2.jpg",
+    category: "Entrevistas",
+    date: "24 de noviembre de 2025",
+    fullContent: `A las puertas de una nueva campaña ... (texto completo)`,
+  },
+];
