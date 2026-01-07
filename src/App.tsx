9a4b8dd4c925e922a1540bc30b9532abacc6a20c
Migueltri <mitrijurrea@gmail.com>
2026-01-07 13:53:19 +0100

    Update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminPage from './pages/admin/gestor';
+import AdminPage from './pages/admin/gestor'; 
 import Home from './pages/home/page';
+// IMPORTANTE: Asegúrate de importar Noticia si lo vas a usar en la línea 10
+// import Noticia from './páginas/noticia/page'; 
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/noticia/:id" element={<Noticia />} />
+        <Route path="/admin" element={<AdminPage />} />
+        {/* Descomenta la siguiente línea cuando tengas el import de Noticia arriba */}
+        {/* <Route path="/noticia/:id" element={<Noticia />} /> */}
       </Routes>
     </BrowserRouter>
   );
