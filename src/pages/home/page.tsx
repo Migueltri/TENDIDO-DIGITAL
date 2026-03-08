03445e85c1f9977bfadba7469c0c2bf2ed673e10
Migueltri <mitrijurrea@gmail.com>
2026-03-08 21:50:12 +0100

    Update page.tsx

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -14329,7 +14329,6 @@ Noticias Guardadas
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6"/></svg>
             </button>
-            
             <button
   onClick={(e) => {
     e.stopPropagation();
