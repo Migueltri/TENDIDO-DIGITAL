43e0b38aadd8baf3caabf0149e2332c0385f3fef
Migueltri <mitrijurrea@gmail.com>
2026-03-08 18:21:54 +0100

    Update page.tsx

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -14343,8 +14343,8 @@ Noticias Guardadas
         )}
               <div className="relative overflow-hidden">
                 <img
-                  src= image
-                  alt= title
+                  src={post.image}
+                  alt={post.title}
                   className="w-full h-48 object-cover object-top group-hover:scale-110 transition-transform duration-500"
                   loading="lazy"
                 />
